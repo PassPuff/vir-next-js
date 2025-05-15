91795b6fdf03af17154fded5b45fb91c4ead30a9
Nikita <passpuffnikita@gmail.com>
2025-05-15 17:03:03 +0300

    feat: интеграция компонента PhoneInput с улучшенной обработкой ввода и стилями

--- src/components/shared/forms/form-basic.tsx
+++ src/components/shared/forms/form-basic.tsx
@@ -1,14 +1,12 @@
 import {
   Form,
   FormControl,
-  // FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { PhoneInput } from "@/components/ui/phone-input";
 import { Button } from "@/components/ui/button";
 import * as z from "zod";
 import { toast } from "sonner";
@@ -22,6 +20,9 @@ import {
   MultiSelectorList,
   MultiSelectorTrigger,
 } from "@/components/ui/multi-select";
+import PhoneInput from "react-phone-input-2";
+import "@/components/react-phone-input-2/style.css";
+import { cn } from "@/lib/utils";
 
 export function FormBasic() {
   // Define the schema using Zod
@@ -124,10 +125,28 @@ export function FormBasic() {
               </FormLabel>
               <FormControl>
                 <PhoneInput
-                  international
-                  defaultCountry="NL"
-                  placeholder="Enter Phone Number..."
+                  country="nl"
+                  inputProps={{
+                    required: true,
+                    autoFocus: true,
+                  }}
+                  preferredCountries={[
+                    "nl",
+                    "de",
+                    "fr",
+                    "es",
+                    "it",
+                    "en",
+                    "us",
+                    "ge",
+                  ]}
                   {...field}
+                  regions={"europe"}
+                  inputClass={cn(
+                    "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+                    "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+                    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+                  )}
                 />
               </FormControl>
               <FormMessage className="text-sm font-normal text-red-600" />
@@ -148,7 +167,6 @@ export function FormBasic() {
                 <MultiSelector
                   values={field.value}
                   onValuesChange={field.onChange}
-                  loop
                 >
                   <MultiSelectorTrigger>
                     <MultiSelectorInput />
