d51c086c6beb56dda0efcf8978bac6581abfa1d2
Nikita <passpuffnikita@gmail.com>
2025-05-20 15:08:06 +0300

    feat: добавляет поддержку автоматической прокрутки в CarouselWrapper

--- src/components/shared/blocks/carousels/carousel-wrapper.tsx
+++ src/components/shared/blocks/carousels/carousel-wrapper.tsx
@@ -2,22 +2,28 @@
 import { Carousel } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Fade from "embla-carousel-fade";
+import AutoScroll from "embla-carousel-auto-scroll";
 
 export function CarouselWrapper({
   children,
   autoplay = false,
   autoplayDelay = 5000,
   fade = false,
+  playOnInit = false,
 }: {
   children: React.ReactNode;
   autoplay?: boolean;
   autoplayDelay?: number;
   fade?: boolean;
+  playOnInit?: boolean;
 }) {
   const plugins = [];
 
   if (fade) {
     plugins.push(Fade());
+  }
+  if (playOnInit) {
+    plugins.push(AutoScroll({ playOnInit: true, speed: 1 }));
   }
 
   if (autoplay) {
--- src/components/shared/blocks/section-marquee.tsx
+++ src/components/shared/blocks/section-marquee.tsx
@@ -7,7 +7,7 @@ export const SectionMarque = () => {
       <h2 className="sr-only">Partners</h2>
       <div className="relative">
         <div className="absolute left-0 top-0 right-0 bottom-0 z-10 w-full h-full bg-gradient-to-r from-background via-white/0 to-background " />
-        <CarouselWrapper autoplay={true} autoplayDelay={1500}>
+        <CarouselWrapper autoplay={true} playOnInit={true}>
           <CarouselContent>
             {Array.from({ length: 25 }).map((_, index) => (
               <CarouselItem className="basis-1/4 lg:basis-1/6" key={index}>
